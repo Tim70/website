99d44a3793e38c27655db1fdf59121af5e7bdc00
Timafei Hushchyn <thushchyn@umass.edu>
2023-06-26 23:49:38 -0400

    funny lil airplane mode thing

--- index.js
+++ index.js
@@ -6,3 +6,9 @@ function getTime(){
 }
 
 setInterval(getTime,1000);
+window.addEventListener('offline', () => {
+    document.body.parentElement.classList.add("offline");
+});
+window.addEventListener("online", () => {
+    document.body.parentElement.classList.remove("offline");
+});
