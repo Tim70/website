7be15a6b436b04d5ea56e89b8aad88a12586b532
Timafei Hushchyn <thushchyn@umass.edu>
2023-07-04 00:35:21 -0400

    weather!

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 
-function getTime(){
+function getTime(weatherText){
     let x = new Date();
     let marquee = document.getElementById("time");
-    marquee.innerText = "The time is " + x.toLocaleTimeString();
+    marquee.innerText = "The time is " + x.toLocaleTimeString() + "|"  + weatherText;
 }
 
-setInterval(getTime,1000);
+async function weather() {
+    const response = await fetch("https://wttr.in/?format=3&m");
+    return await response.text();
+  }
+
+weather().then( (weatherText) => {
+    setInterval(getTime(weatherText), 1000);
+});
+
+
 window.addEventListener('offline', () => {
     document.body.parentElement.classList.add("offline");
 });
